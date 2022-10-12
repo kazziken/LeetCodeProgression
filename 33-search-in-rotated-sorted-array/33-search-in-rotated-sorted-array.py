53422fb6018e6aa01dc1ac4a8f96047aa1707d01
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-10-12 15:28:52 -0400

    Time: 78 ms (48.61%), Space: 14.3 MB (17.94%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,22 +1,31 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r = 0, len(nums) - 1
-
-        while l <= r:
-            mid = (l + r) // 2
+        """
+        thoughts:
+        to find the pivot point by binary search then go for the correct half to perform another round of binary search 
+        until target is found
+        
+        we need the target to find pivot point
+        
+        """
+        
+        
+        left, right = 0, len(nums) -1 
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
             if target == nums[mid]:
                 return mid
-
-            # left sorted portion
-            if nums[l] <= nums[mid]:
-                if target > nums[mid] or target < nums[l]:
-                    l = mid + 1
+            
+            if nums[left] <= nums[mid]:
+                if target > nums[mid] or target < nums[left]:
+                    left = mid + 1
                 else:
-                    r = mid - 1
-            # right sorted portion
+                    right = mid -1
             else:
-                if target < nums[mid] or target > nums[r]:
-                    r = mid - 1
+                if target < nums[mid] or target > nums[right]:
+                    right = mid -1
                 else:
-                    l = mid + 1
+                    left = mid + 1
         return -1
