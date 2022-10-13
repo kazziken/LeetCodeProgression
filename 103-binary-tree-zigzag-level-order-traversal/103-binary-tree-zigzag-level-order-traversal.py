223dc85d0ab9955c3c439f3069d2ba9f61f8c306
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-10-13 15:15:59 -0400

    Time: 83 ms (5.97%), Space: 14.1 MB (56.98%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.py
@@ -9,36 +9,28 @@ from collections import deque
 class Solution:
     def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         
-        result = []
+        res = []
         if root is None:
-            return result
-
-        queue = deque()
-        queue.append(root)
+            return []
+        
+        queue = deque([root])
         leftToRight = True
+        
         while queue:
-            levelSize = len(queue)
             currentLevel = deque()
-            for _ in range(levelSize):
+            for i in range(len(queue)):
                 currentNode = queue.popleft()
-
-              # add the node to the current level based on the traverse direction
                 if leftToRight:
                     currentLevel.append(currentNode.val)
                 else:
                     currentLevel.appendleft(currentNode.val)
-
-                  # insert the children of current node in the queue
                 if currentNode.left:
                     queue.append(currentNode.left)
                 if currentNode.right:
                     queue.append(currentNode.right)
-
-            result.append(list(currentLevel))
-            # reverse the traversal direction
+            res.append(list(currentLevel))
             leftToRight = not leftToRight
-
-        return result
+        return res
             
                 
         
