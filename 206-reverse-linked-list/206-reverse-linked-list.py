841b5e0952c42d2d2612abd5d07599918131585c
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-10-15 11:21:26 -0400

    Time: 31 ms (98.82%), Space: 15.5 MB (27.27%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -11,8 +11,11 @@ class Solution:
         while current:
             #set old current.next to a variable
             next = current.next
-            #
+            # current.next is now previous
             current.next = previous
+            # previous now is the current
             previous = current
+            # current is now current.next
             current = next
+            #repeat til current is null that is pointing at the head
         return previous
