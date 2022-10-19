d4dc6ee8cedac69a34587d275b392dee93ae5842
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-10-19 19:41:40 -0400

    Time: 36 ms (85.73%), Space: 13.9 MB (57.02%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,9 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        a, b = 1, 0
+#         a, b = 1, 0
         
-        for _ in range(n):
-            a, b = b+a, a
-        return a
+#         for _ in range(n):
+#             a, b = b+a, a
+#         return a
         
+    #O(n) space
+    
+        if n == 1:
+            return 1
+        #create empty list
+        res = [0 for i in range(n)]
+        res[0], res[1] = 1,2
+        
+        for i in range(2,n):
+            res[i] = res[i-1] + res[i-2]
+        return res[-1]
         
