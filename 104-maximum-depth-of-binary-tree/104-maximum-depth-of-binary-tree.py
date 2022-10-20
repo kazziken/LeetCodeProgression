5747b29caa7a47c0eaa9d08c94afb1bfd6cf3d2a
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-10-20 14:44:04 -0400

    Time: 89 ms (27.39%), Space: 15.3 MB (80.92%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -4,28 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+from collections import deque
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        
+#         if root is None:
+#             return 0
+        
+#         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+    
+    
+    #solving using Depth First Search(DFS) - using a Deque
         
         if root is None:
             return 0
         
-        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-    
-    
-    #solving using Depth First Search(DFS) - using a STACK
+        queue = deque([root])
+        nodeLevel = 1
+        levels = 0
         
-#         stack = [[root, 1]] #depth is 1
-#         result = 0
-        
-#         while stack:
-#             node, depth = stack.pop()
-            
-#             if node:
-#                 result = max(result, depth)
-#                 stack.append([node.left, depth + 1])
-#                 stack.append([node.right, depth + 1])
-                
-#         return result
+        while queue:
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            # subtract from nodeLevelwhile adding to levels to represent the BFS
+            nodeLevel -=1
+            if nodeLevel == 0:
+                levels +=1
+                nodeLevel = len(queue)
+        return levels
         
         
