7ea34b6f2a264f5a05fbc06f8a2d9f17d0262c20
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-10-24 01:37:46 -0400

    Time: 225 ms (81.14%), Space: 15.6 MB (65.02%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        """
+        If we do fast and slow pointer
+        slow pointer start at 0
+        and slow pointer moving depending on the length of the list
+        make fast pointer determine switching the numbers til all the 0s are in the beginning of the array
+        and the rest of the nums are compared based on value
+        """
+        
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow +=1
+                    
+        #[1,2,3,0,0,4,5]
+                #^slow
+        
+        
