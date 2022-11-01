e1d0ac4080e6fc20380f021e1888c02895b06fd7
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-10-31 20:13:16 -0400

    Time: 804 ms (89.16%), Space: 27.4 MB (44.58%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -18,18 +18,18 @@ class Solution:
 #         this was TLE
 
         #lets use a two pointer technique, one at the right, one at the left LIKE WE THOUGHT
-        maxArea = 0
-        l, r = 0, len(height) - 1
-        
-        while l < r:
-            area = (r - l) * min(height[l], height [r])
-            maxArea = max(area, maxArea)
+            maxArea = 0
+            l, r = 0, len(height) - 1
             
-            if height[l] < height[r]:
-                l += 1
-            else:
-                r -= 1
-        
-        return maxArea    
+            while l < r:
+                area = (r - l) * min(height[l], height[r])
+                
+                maxArea = max(area, maxArea)
+                
+                if height[l] < height[r]:
+                    l +=1
+                else:
+                    r -=1
+            return maxArea
                                 
         
