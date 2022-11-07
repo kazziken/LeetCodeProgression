486f23bff1c0133b65b78f79ec40cfdb4f626e17
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-11-07 15:26:32 -0500

    Time: 43 ms (86.66%), Space: 15.6 MB (26.70%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.py
+++ 206-reverse-linked-list/206-reverse-linked-list.py
@@ -5,17 +5,18 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        previous = None
-        current = head
+        current, prev = head, None
         
         while current:
             #set old current.next to a variable
             next = current.next
-            # current.next is now previous
-            current.next = previous
+            # current.next is now previous WHICH IS NONE so it looks like 
+            # null <- 1 -> 2 -> 3 -> 4
+        #current.next 
+            current.next = prev
             # previous now is the current
-            previous = current
+            prev = current
             # current is now current.next
             current = next
             #repeat til current is null that is pointing at the head
-        return previous
+        return prev
