6e31096cd74b5d8675206d7ca758b6ecc1c9246e
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-11-10 18:49:25 -0500

    Time: 76 ms (6.06%), Space: 14 MB (31.92%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -4,7 +4,16 @@ class Solution:
                    '6': 'mno' , '7': 'pqrs', '8': 'tuv', '9': 'wxyz',
         }
         
-        cmb = [''] if digits else []
-        for d in digits:
-            cmb = [p + q for p in cmb for q in mapping[d]] #loop counter
-        return cmb
+        # cmb = [''] if digits else []
+        # for d in digits:
+        #     cmb = [p + q for p in cmb for q in mapping[d]] #loop counter
+        # return cmb
+        
+        if len(digits) == 0:
+            return []
+        if len(digits) == 1:
+            return list(mapping[digits[0]])
+        prev = self.letterCombinations(digits[:-1]) #all the elements except last one
+        additional = mapping[digits[-1]] #last element of digit
+        return [s + c for s in prev for c in additional] # loop counter
+    
