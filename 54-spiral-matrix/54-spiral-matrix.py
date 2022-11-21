beb0e3bcd4887b2401dba9b287fea3012caa668f
Kenneth Mei <99045224+kazziken@users.noreply.github.com>
2022-11-21 18:08:50 -0500

    Time: 42 ms (78.51%), Space: 13.9 MB (34.75%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -1,24 +1,35 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         res = []
-        if len(matrix) == 0: return res
         
-        row_begin = col_begin = 0
-        row_end = len(matrix) - 1 
-        col_end = len(matrix[0]) - 1
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix) 
         
-        while (row_begin <= row_end and col_begin <= col_end):
-            res += [matrix[row_begin][col] for col in range(col_begin, col_end + 1)]
-            row_begin += 1
-            res += [matrix[row][col_end] for row in range(row_begin, row_end + 1)]
-            col_end -= 1
+        while left < right and top < bottom:
+            # left to right
+            # get every value in top row
             
-            if (row_begin <= row_end):
-                res += [matrix[row_end][col] for col in range(col_end, col_begin - 1, -1)]
-                row_end -= 1
-                
-            if (col_begin <= col_end):
-                res += [matrix[row][col_begin] for row in range(row_end, row_begin - 1, -1)]
-                col_begin += 1
-        
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1 # this is shifting top down one
+            
+            for i in range(top, bottom):
+                res.append(matrix[i][right - 1]) #because right is out of bounds
+            right -= 1
+            
+            if not(left < right and top < bottom):
+                break
+            
+            for i in range(right - 1, left -1, -1): #dont want to include already traversed elements
+                res.append(matrix[bottom - 1][i])
+            bottom -= 1
+            
+            for i in range(bottom - 1, top - 1, -1):
+                res.append(matrix[i][left])
+            left += 1
+            
         return res
+                
+                
+            
+            
